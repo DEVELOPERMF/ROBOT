fc97de4bb5b45fd2467091e834dcc9c499bc30f3
DEVELOPERMF <kamil.sopata.snv@gmail.com>
2015-06-29 14:32:08 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -5,6 +5,7 @@ var emojibuttonon = $(".icon-emoji-on");
 if (emojibuttonon.length > 0) {
 	emojibuttonon[0].click();
 }
+API.chatLog('Emotikony offline.');
 var onlyaudio = $(".item.s-vo");
 if (onlyaudio.length > 0) {
 	onlyaudio[0].click();
